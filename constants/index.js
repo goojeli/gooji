e5e9ea376270dabe9dd4a187034e196c7777c6a9
goojeli <118731822+goojeli@users.noreply.github.com>
2023-07-31 12:40:49 -0400

    Update index.js

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0x69028a80b3018a8cbc575065b37f6680cae329b6";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
